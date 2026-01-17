831588125ff9320bdd83b3d37a517b66c031862a
OmezzolliMarco <omezzollimarco@gmail.com>
2026-01-17 09:53:58 +0100

    Lezione del 17/01/2026

--- tkinter/Tic Tac Toe/tictactoe.py
+++ tkinter/Tic Tac Toe/tictactoe.py
@@ -1,5 +1,32 @@
 import tkinter as tk
 #funzioni
+
+def resettaGioco():
+    global board, bottoni, player
+    player = "X"
+    board = [["" for i in range(3)] for j in range(3)]
+
+    #grafica
+    generale = tk.PhotoImage(file="tkinter\\Tic Tac Toe\\general.png")
+    generale_piccolo = generale.subsample(8,8)
+    #inizio la generazione dei pulsanti
+    for i in range(0, 3):
+        for j in range(0, 3):
+            bottoni[i][j].config(image=generale_piccolo)
+            bottoni[i][j].image=generale_piccolo
+
+def controllaVincitore():
+    global board
+    global vittorie
+    for v in vittorie:
+        (a,b) = v[0]
+        (c,d) = v[1]
+        (e,f) = v[2]
+        if board[a][b] != "" and board[a][b] == board[c][d] == board[e][f]:
+            #chi ha riempito queste posizioni ha vinto
+            return board[a][b] #ritorna X oppure O
+    return None
+
 def gioca(riga:int, colonna:int):
     icona1_originale = tk.PhotoImage(file="tkinter\\Tic Tac Toe\\cat.png")
     icona2_originale = tk.PhotoImage(file="tkinter\\Tic Tac Toe\\dog.png")
@@ -27,6 +54,18 @@ def gioca(riga:int, colonna:int):
         bottoni[riga][colonna].image = icona2
     
     #controllo del vincitore
+    vincitore = controllaVincitore()
+    if vincitore != None:
+        if vincitore == "X":
+            print("Vince player 1")
+        else:
+            print("Vince player 2")
+        resettaGioco()
+
+    #controllo del pareggio
+    if all(board[i][j]!="" for i in range(3) for j in range(3)):
+        print("Pareggio!")
+        resettaGioco()
 
     #cambio del player
     if player=="X":
@@ -34,6 +73,24 @@ def gioca(riga:int, colonna:int):
     else:
         player = "X"
 
+
+
+#main
+
+
+vittorie = [
+    #righe
+    [(0,0), (0,1), (0,2)],
+    [(1,0), (1,1), (1,2)],
+    [(2,0), (2,1), (2,2)],
+    #colonne
+    [(0,0), (1,0), (2,0)],
+    [(0,1), (1,1), (2,1)],
+    [(0,2), (1,2), (2,2)],
+    #diagonali
+    [(0,0), (1,1), (2,2)],
+    [(0,2), (1,1), (2,0)]
+]
 
 #main
 #definizione elementi del gioco
